0180fe1c197737d06b4a95e54da690ade3f7ff4e
JiaRanToday <3206106661@qq.com>
2024-12-23 09:18:30 +0800

    doublyLinklist

--- datastruct/Doublelinklist.c
+++ datastruct/Doublelinklist.c
@@ -1,5 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef int ElemType;
 typedef struct Node {
@@ -8,7 +10,33 @@ typedef struct Node {
   struct Node *next;
 } Node;
 
-typedef enum { ERROR = -1, FALSE, TRUE } Status;
-typedef Node *DoubleLinkList;
+typedef struct {
+  Node *head;
+  Node *tail;
+} DoublyLinkList;
 
-Status GetElem()
+typedef enum { ERROR = -1, FALSE, TRUE } Status;
+
+void InitList(DoublyLinkList *list) {
+  if (list == NULL) {
+    list = (DoublyLinkList *)malloc(sizeof(DoublyLinkList));
+  }
+  list->head = NULL;
+  list->tail = NULL;
+}
+Status InsertHead(DoublyLinkList *list, ElemType data) {
+  if (list == NULL) {
+    return ERROR;
+  }
+  Node *newNode = (Node *)malloc(sizeof(Node));
+  newNode->data = data;
+  newNode->prior = NULL;
+  newNode->next = list->head;
+  if (list->head != NULL) {
+    list->head->prior = newNode;
+  } else {
+    list->tail = newNode;
+  }
+  list->head = newNode;
+  return TRUE;
+}
